a57a79990b8af5497a3d181e9caaef378358b202
Sohee Im <imsophia0313@gmail.com>
2018-08-09 16:33:18 +1200

    form insert database

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var validateInput = require('../../../src/shared/validations/signup')
+var validateInput = require('../../shared/validations/signup')
 
 // router.post("/", function(req, res){
 //     console.log(req.body);
@@ -9,15 +9,15 @@ var validateInput = require('../../../src/shared/validations/signup')
 
 
 router.post("/", function(req, res){
-    
-    setTimeout(() => {
-        const { errors , isValid } = validateInput(req.body);
+        // const { errors } = validateInput(req.body);
         
-        if(!isValid) {
-            res.status(400).json(errors);
-        }
-    },1000)
-    
+        // if(!isValid) {
+        //     res.status(400).json(errors);
+        // }
+        res.locals.connection.query('insert into users(name, email, password) values("'+req.body.username+'", "'+req.body.email+'", "'+req.body.password+'" '), function (error, results, fields) {
+            if(error) throw error;
+            res.send(JSON.stringify(results));
+        });
 });
 //router.post("/login", controller.login);
 
