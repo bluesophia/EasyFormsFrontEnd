3116ca8f6247f000c4a6af5e5f13296be2f0af37
yun <jeongyunw@gmail.com>
2018-08-09 16:51:16 +1200

    Home Section 04 responsive

--- src/Client/Components/WebContainer/HomeContainer/Section4/index.js
+++ src/Client/Components/WebContainer/HomeContainer/Section4/index.js
@@ -5,9 +5,11 @@ import Themes from '../../../../../Assets/Styles/Themes';
 import HeaderLine from '../../../Common/HeaderLine';
 import Title from '../../../Common/Title/Title';
 
-import Section04BgSm from '../../../../../Assets/Images/easyform-functions_mobile.png';
+import Section04BgSm from '../../../../../Assets/Images/easyform-functions_mobileBg.png';
 import Section04BgLg from '../../../../../Assets/Images/easyform-functions_webBg.png';
 import Section04Md from '../../../../../Assets/Images/easyform-functions_tablet.png';
+import Section04ImageSm from '../../../../../Assets/Images/easyform-functions_mobileImage.png';
+import Section04ImageMd from '../../../../../Assets/Images/easyform-functions_tabletImage.png';
 import Section04ImageLg from '../../../../../Assets/Images/easyform-functions_webImage.png';
 
 class Section4 extends Component{
@@ -16,10 +18,11 @@ class Section4 extends Component{
             <Section04>
                 <Section04__TitleDiv>
                     <Section04__Title>Easyforms Function</Section04__Title>
-                    {/* <HeaderLine /> */}
                 </Section04__TitleDiv>
                 <Section04__ImageDiv>
-                    <Section04__Image src={Section04ImageLg} />
+                    <Section04__ImageSm src={Section04ImageSm} />
+                    <Section04__ImageMd src={Section04ImageMd} />
+                    <Section04__ImageLg src={Section04ImageLg} />
                 </Section04__ImageDiv>
             </Section04>
         )
@@ -30,20 +33,17 @@ class Section4 extends Component{
 
 const Section04 = styled.div`
     background: url('${Section04BgSm}') no-repeat center bottom;
-    background-size:auto 100%;
-    height:1226px;
+    background-size:100% 100%;
+    height:1100px;
+    position:relative;
     ${breakpoint('md')`
-     background: url('${Section04Md}') no-repeat;
-     background-position: center bottom;
-     background-size:auto 100%;
-     height:845px;
+     height:720px;
     `}
-    ${breakpoint('md')`
+    ${breakpoint('lg')`
     background: url('${Section04BgLg}') no-repeat;
     background-position: center bottom;
     background-size:auto 100%;
     height:768px;
-    position:relative;
    `}
 `
 const Section04__TitleDiv = styled.div`
@@ -52,27 +52,50 @@ const Section04__TitleDiv = styled.div`
         display:flex;
         flex-direction:column;
         align-items:center;
-        // justify-content:flex-end;
     `}
 `
 const Section04__Title = styled.div`
    ${Title};
    color:white;
+   padding-top:100px;
     ${breakpoint('lg')`
         padding-top:150px;
     `}
 `
 const Section04__ImageDiv = styled.div`
-    display:none;
-    ${breakpoint('lg')`
-        display:block;
-        margin:0 auto;
-    `}
+    margin:0 auto;
 `
-const Section04__Image = styled.img`
+const Section04__ImageSm = styled.img`
+    display:block;
     position:absolute;
     bottom:-170px;
     left:50%;
     transform:translateX(-50%);
+    ${breakpoint('md')`
+        display:none;
+    `}
+`
+const Section04__ImageMd = styled.img`
+    display:none;
+    ${breakpoint('md')`
+        display:block;
+        position:absolute;
+        bottom:-170px;
+        left:50%;
+        transform:translateX(-50%);   
+    `}
+    ${breakpoint('lg')`
+        display:none;
+    `}
+`
+const Section04__ImageLg = styled.img`
+    display:none;
+    ${breakpoint('lg')`
+        display:block;
+        position:absolute;
+        bottom:-170px;
+        left:50%;
+        transform:translateX(-50%);   
+    `}
 `
 export default Section4;
